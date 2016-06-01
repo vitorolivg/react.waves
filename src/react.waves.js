d71201edd4003ac8019266e8cfc1955defa80c4a
andrei gec <andreigec@hotmail.com>
2016-06-01 13:46:44 +1000

    wip

---
+++ src/react.waves.js
@@ -0,0 +1,36 @@
+import React, {
+	Component
+}
+from 'react';
+
+export default class Waves extends Component {
+
+	componentDidMount = () => {
+		this.init()
+	};
+
+	init = () => {
+		// make some waves.
+		var ocean = document.getElementById("ocean"),
+			waveWidth = 10,
+			waveCount = Math.floor(window.innerWidth / waveWidth),
+			docFrag = document.createDocumentFragment();
+
+		for (var i = 0; i < waveCount; i++) {
+			var wave = document.createElement("div");
+			wave.className += " wave";
+			docFrag.appendChild(wave);
+			wave.style.left = i * waveWidth + "px";
+			wave.style.webkitAnimationDelay = (i / 100) + "s";
+		}
+
+		ocean.appendChild(docFrag);
+	}
+
+	render() {
+
+		return (
+	  <div id="ocean"></div>
+		)
+	};
+}
